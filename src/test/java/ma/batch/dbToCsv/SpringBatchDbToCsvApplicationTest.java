a8e9244a7fdc663fef182fbaaed5cbcbca848d10
migorman <mounir.igorman@gmail.com>
2017-12-15 00:31:27 +0000

    test

---
+++ src/test/java/ma/batch/dbToCsv/SpringBatchDbToCsvApplicationTest.java
@@ -0,0 +1,14 @@
+package ma.batch.dbToCsv;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SpringBatchDbToCsvApplicationTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
